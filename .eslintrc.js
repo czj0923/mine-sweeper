034d957b22b40ac5ea04098601d3465f98321368
Cao Zhijie <1051029537@qq.com>
2022-04-23 10:52:37 +0800

    refactor 使用ts重构项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/"
-      ><h4><span class="badge secondary">扫雷小游戏（mine-sweeper）</span></h4>
-    </router-link>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
   <router-view />
-  <Alert></Alert>
 </template>
 
-<script>
-import Alert from "@/components/Alert";
-import { useMainStore } from "./store/main";
-export default {
-  name: "App",
-  components: {
-    Alert,
-  },
-  setup() {
-    const store = useMainStore();
-    //初始化时读取本地存储中的纪录
-    const record = JSON.parse(localStorage.getItem("record"));
-    store.refreshRecord(record);
-  },
-};
-</script>
-
 <style lang="scss">
-* {
-  user-select: none;
-  padding: 0;
-  margin: 0;
-}
-body {
-  background-image: url(./assets/images/bg.webp);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 20px;
-  text-align: left;
-}
-.panel {
-  background-color: #fff;
-  margin-bottom: 20px;
-  padding: 20px;
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
